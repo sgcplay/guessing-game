b551d87965b7e8d6033579a2c1c3d2583215a07d
sgcplay <frolkin.chepurnov@gmail.com>
2017-09-21 13:03:12 +0300

    100% passing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.imax = 0;
+        this.imin = 0;
+        this.imid = 0;
+    }
 
     setRange(min, max) {
-
+        this.imin = min;
+        this.imax = max;
     }
 
     guess() {
-
+        this.imid = Math.ceil(this.imin + (this.imax - this.imin) / 2);
+        return this.imid;
     }
 
     lower() {
-
+        this.imax = this.imid;  
     }
 
     greater() {
-
+        this.imin = this.imid;
     }
 }
 
